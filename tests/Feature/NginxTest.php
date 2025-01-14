cbe7cac8e10f54b8aa886060fe9ea7b82592425c
Aaron Bushnell <aaron.bushnell@gmail.com>
2025-01-14 10:26:27 -0500

    Support multiline IP restrictions

--- tests/Feature/NginxTest.php
+++ tests/Feature/NginxTest.php
@@ -16,3 +16,16 @@ test('ip formatting does not matter', function () {
     $updatedNginx = Nginx::setAllowedIps($baseNginx, '9.9.9.9');
     expect($updatedNginx)->toMatchSnapshot();
 });
+
+test('new lines are accepted', function () {
+    $ips = "
+        1.1.1.1;
+        2.2.2.2;
+        3.3.3.3;
+    ";
+
+    Config::set('forge.allowed_ips', $ips);
+    $baseNginx = file_get_contents(base_path('tests/Fixtures/nginx.conf'));
+    $updatedNginx = Nginx::setAllowedIps($baseNginx, '9.9.9.9');
+    expect($updatedNginx)->toMatchSnapshot();
+});
